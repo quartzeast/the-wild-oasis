c6c916a8e3221a8d6e1c1ed09a1247a3f8256a0a
shiyingdong <shiyingdong@webull.com>
2025-02-11 17:16:36 +0800

    Uploading Cabin Image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tvnnualxikgrmlkbyngc.supabase.co';
+export const supabaseUrl = 'https://tvnnualxikgrmlkbyngc.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2bm51YWx4aWtncm1sa2J5bmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTQ0ODcsImV4cCI6MjA1MTc5MDQ4N30.hGRII8-cEXsiIivRfRNTN-KyKbOPi1NixORGTvOsNzc';
 
