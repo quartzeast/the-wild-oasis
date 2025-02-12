5d99008da4a98af3011723a78bd30734af4b9a84
shiyingdong <shiyingdong@webull.com>
2025-02-12 15:56:09 +0800

    Prefetching With React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router';
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -12,7 +14,7 @@ export function useBookings() {
       ? null
       : { field: 'status', value: filterValue };
 
-  //SORT
+  // SORT
   const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
   const [field, direction] = sortByRaw.split('-');
   const sortBy = { field, direction };
@@ -20,6 +22,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -28,6 +31,21 @@ export function useBookings() {
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, error, bookings, count };
 }
