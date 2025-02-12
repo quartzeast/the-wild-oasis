2b2eb8c3770d939b977c5c70751c55de6a1e052f
shiyingdong <shiyingdong@webull.com>
2025-02-12 15:39:22 +0800

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,20 +5,25 @@ import { getBookings } from '../../services/apiBookings';
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  // FILTER
   const filterValue = searchParams.get('status');
   const filter =
     !filterValue || filterValue === 'all'
       ? null
       : { field: 'status', value: filterValue };
-  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  //SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
