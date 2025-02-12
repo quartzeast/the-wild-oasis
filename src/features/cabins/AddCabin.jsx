fd825ccdd4cbef04bbe5b2e26554b9719691765a
shiyingdong <shiyingdong@webull.com>
2025-02-12 10:47:44 +0800

    Detecting a Click Outside the Modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,6 +1,5 @@
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
-import CabinTable from './CabinTable';
 import CreateCabinForm from './CreateCabinForm';
 
 function AddCabin() {
@@ -11,13 +10,6 @@ function AddCabin() {
       </Modal.Open>
       <Modal.Window name='cabin-form'>
         <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens='table'>
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name='table'>
-        <CabinTable />
       </Modal.Window>
     </Modal>
   );
