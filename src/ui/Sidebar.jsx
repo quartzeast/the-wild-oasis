a3e193a6283a242416697b85248a7aa639b5ada5
quartzeast <quartzeast@gmail.com>
2025-02-12 23:01:54 +0800

    Adding Social Links

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import Uploader from '../data/Uploader';
 import Logo from './Logo';
 import MainNav from './MainNav';
+import SocialLinks from './SocialLinks';
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -19,7 +19,7 @@ function Sidebar() {
       <Logo />
       <MainNav />
 
-      <Uploader />
+      <SocialLinks />
     </StyledSidebar>
   );
 }
