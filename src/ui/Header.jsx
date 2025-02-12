d2b6e45ad1b49697b2a6b49681a538d3bea9de4f
shiyingdong <shiyingdong@webull.com>
2025-02-12 18:02:20 +0800

    Building The App Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,29 @@
 import styled from 'styled-components';
-import Logout from '../features/authentication/Logout';
+import UserAvatar from '../features/authentication/UserAvatar';
+import HeaderMenu from './HeaderMenu';
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const HamburgerButton = styled.button`
+  background-color: transparent;
+  padding: 0.5rem 1rem;
+  border: none;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
